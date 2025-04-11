96ca17dac6430706957490072941fb131e5e98d0
thiagodau <thiagorodriguesdau@gmail.com>
2025-04-11 19:26:26 -0400

    a

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,11 +3,12 @@ import { pool } from "../db/index.js";
 
 const router = express.Router();
 
-router.get("u/:google_id", async (req, res) => {
+router.get("/u/:google_id", async (req, res) => {
   const { google_id } = req.params;
 
   try {
-    const result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
+    const result = await pool.query("SELECT name, email FROM users");
+    //const result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
 
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Usuário não encontrado." });
@@ -18,7 +19,6 @@ router.get("u/:google_id", async (req, res) => {
     res.json({
       name,
       email,
-      links: [] // você pode substituir por dados reais depois
     });
   } catch (err) {
     console.error("Erro ao buscar usuário:", err);

0d02aa36cb10f440f471786768b9aef887226e83
thiagodau <thiagorodriguesdau@gmail.com>
2025-04-16 11:01:49 -0400

    adjusted query user

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,8 +10,12 @@ router.get("/u/:google_id", async (req, res) => {
     const result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
 
     if (result.rows.length === 0) {
-      console.log(result)
-      return res.status(404).json({ message: "Usuário não- encontrado." });
+      await pool.query(
+        "INSERT INTO users (google_id, name, email) VALUES ($1, $2, $3)",
+        [google_id, 'Novo User', null]
+      );
+      
+      result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
     }
 
     const { name, email } = result.rows[0];

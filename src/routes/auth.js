edb78f9ed0d1e97bb8e8c9c6da7f7f764d3bd010
thiagodau <thiagorodriguesdau@gmail.com>
2025-04-16 10:50:33 -0400

    add user url

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,7 +9,30 @@ router.get("/google", passport.authenticate("google", {
 
 router.get("/google/callback",
   passport.authenticate("google", { failureRedirect: "/auth/failure" }),
-  (req, res) => {
+  async (req, res) => {
+    //res.redirect(`${process.env.FRONT_END_BASE_URL}/u/${req.user.google_id}`);
+
+    try {
+      const result = await pool.query(
+        "SELECT name, email FROM users WHERE google_id = $1",
+        [req.google_id]
+      );
+      if (result.rows.length === 0) {
+        console.log("Usuário não encontrado. Inserindo novo usuário.");
+
+        await pool.query(
+          "INSERT INTO users (google_id, name, email) VALUES ($1, $2, $3)",
+          [req.google_id, req.user.name, req.user.email]
+        );
+
+        //return res.status(201).json({ name, email });
+        return res.redirect(`${process.env.FRONT_END_BASE_URL}/u/${req.user.google_id}`);
+      }
+    }
+    catch (e) {
+      console.log(e + ' DEU ERRO!')
+    }
+
     res.redirect(`${process.env.FRONT_END_BASE_URL}/u/${req.user.google_id}`);
   }
 );
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,31 +5,26 @@ const router = express.Router();
 
 router.get("/u/:google_id", async (req, res) => {
   const { google_id } = req.params;
-  const { name, email } = req.body; // vindo no corpo da requisição
-
+  console.log(google_id)
   try {
-    const result = await pool.query(
-      "SELECT name, email FROM users WHERE google_id = $1",
-      [google_id]
-    );
+    const result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
 
     if (result.rows.length === 0) {
-      console.log("Usuário não encontrado. Inserindo novo usuário.");
-
-      await pool.query(
-        "INSERT INTO users (google_id, name, email) VALUES ($1, $2, $3)",
-        [google_id, name, email]
-      );
-
-      return res.status(201).json({ name, email });
+      console.log(result)
+      return res.status(404).json({ message: "Usuário não- encontrado." });
     }
 
-    const user = result.rows[0];
-    res.json({ name: user.name, email: user.email });
+    const { name, email } = result.rows[0];
+
+    res.json({
+      name,
+      email,
+    });
   } catch (err) {
-    console.error("Erro ao buscar ou inserir usuário:", err);
-    res.status(500).json({ message: "Erro ao buscar ou inserir usuário." });
+    console.error("Erro ao buscar usuário:", err);
+    res.status(500).json({ message: "Erro ao buscar usuário." });
   }
 });
+
 
 export default router;

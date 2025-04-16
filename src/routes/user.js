254ab6d7f77a455a99eedc2304f1e863d327e6c9
thiagodau <thiagorodriguesdau@gmail.com>
2025-04-16 09:40:57 -0400

    add query

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,8 +7,7 @@ router.get("/u/:google_id", async (req, res) => {
   const { google_id } = req.params;
 
   try {
-    const result = await pool.query("SELECT name, email FROM users");
-    //const result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
+    const result = await pool.query("SELECT name, email FROM users WHERE google_id = $1", [google_id]);
 
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Usuário não encontrado." });
